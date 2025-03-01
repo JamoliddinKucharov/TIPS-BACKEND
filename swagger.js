dd518b132984b8cdf70760e824444d9724fd19fc
JamoliddinKucharov <jamoliddindev@gmail.com>
2025-03-02 00:58:01 +0500

    feat: mini updates

--- swagger.js
+++ swagger.js
@@ -1,27 +1,27 @@
-const swaggerJSDoc = require('swagger-jsdoc');
-const swaggerUi = require('swagger-ui-express');
+const swaggerJSDoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
 
 const options = {
   definition: {
-    openapi: '3.0.0',
+    openapi: "3.0.0",
     info: {
-      title: 'Node.js API Documentation',
-      version: '1.0.0',
-      description: 'This is the API documentation for my Node.js project',
+      title: "Node.js API Documentation",
+      version: "1.0.0",
+      description: "This is the API documentation for my Node.js project",
     },
     servers: [
       {
-        url: 'https://tips-backend-one.vercel.app', 
+        url: "https://tips-backend-one.vercel.app",
       },
     ],
   },
-  apis: ['./routes/*.js'], // API'laringiz joylashgan papka
+  apis: ["./routes/*.js"],
 };
 
 const swaggerSpec = swaggerJSDoc(options);
 
 function setupSwagger(app) {
-  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 }
 
 module.exports = setupSwagger;
