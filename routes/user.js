03e541df5daaf31e119f5da5cc35876ce2b11775
JamoliddinKucharov <jamoliddindev@gmail.com>
2025-03-18 00:17:16 +0500

    feat: added account find and edit account

--- routes/user.js
+++ routes/user.js
@@ -1,35 +1,24 @@
 const express = require("express");
 const User = require("../models/User");
 const jwt = require("jsonwebtoken");
+const { updateUser, getAccount } = require("../controllers/userController");
+const { body } = require("express-validator");
 
 const router = express.Router();
- 
-router.get("/account", async (req, res) => {
-  const token = req.headers.authorization?.split(" ")[1];  
 
-  if (!token) {
-    return res.status(401).json({ message: "No token provided" });
-  }
+router.get("/account", getAccount);
 
-  try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
-
-    const user = await User.findById(decoded.userId);
-
-    if (!user) {
-      return res.status(404).json({ message: "User not found" });
-    }
-
-    res.status(200).json({ message: "User found", user });
-  } catch (error) {
-    console.error("Error verifying token:", error);
-
-    if (error.name === "TokenExpiredError") {
-      return res.status(401).json({ message: "Token expired" });
-    }
-
-    res.status(500).json({ message: "Internal Server Error" });
-  }
-});
+// User Update
+router.put(
+  "/account/:userId",
+  [
+    body("email").isEmail().withMessage("Invalid email address"),
+    body("phone")
+      .optional()
+      .isMobilePhone()
+      .withMessage("Invalid phone number"),
+  ],
+  updateUser
+);
 
 module.exports = router;
